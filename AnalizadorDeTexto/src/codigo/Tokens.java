42cc7797f5dcacb5f9ba8e78223fd998332ad2da
alemor026 <78941341+alemor026@users.noreply.github.com>
2021-09-19 21:43:37 -0600

    analizador de texto

---
+++ AnalizadorDeTexto/src/codigo/Tokens.java
@@ -0,0 +1,18 @@
+package codigo;
+
+/**
+ *
+ * @author Alejandra Morales
+ * 
+ */
+public enum Tokens {
+    Reservadas,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Identificador,
+    Numero,
+    ERROR
+}
